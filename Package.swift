ceb6eb24bf905b7c4abc50a30fc493e14f88ca61
yossibank <yossibank@yahoo.co.jp>
2025-11-29 21:57:45 +0900

    Update Package.swift for v1.0.0 with Asset ID 322210066

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version:5.9
-
 import PackageDescription
 
 let package = Package(
@@ -15,9 +14,9 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(
-            name: "SharedLibrary",
-            url: "https://api.github.com/repos/yossibank/KotlinTemplate/releases/assets/assetID.zip",
-            checksum: "checksum"
+            name: "KotlinMultiplatformLibrary",
+            url: "https://api.github.com/repos/yossibank/KotlinTemplate/releases/assets/322210066.zip",
+            checksum: "3f6dc41f8c8ff029ab4df589d26093953fe8a53e4c8e4eace8dee99fea451d19"
         )
     ]
 )
