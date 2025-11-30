19bc3d23152531dd793edb0c29a620ce03de9d12
yossibank <yossibank@yahoo.co.jp>
2025-11-30 18:18:40 +0900

    Fix Test

--- kotlinMultiplatformLibrary/src/commonTest/kotlin/foundation/ValueConverterTest.kt
+++ kotlinMultiplatformLibrary/src/commonTest/kotlin/foundation/ValueConverterTest.kt
@@ -1,6 +1,7 @@
 package foundation
 
 import com.example.kotlinmultiplatformlibrary.foundation.ValueConverter
+import com.example.kotlinmultiplatformlibrary.foundation.ValueFormat
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -14,6 +15,30 @@ class ValueConverterTest {
         // act
         val actual = ValueConverter().format(
             formatter = ValueConverter.Formatter(value = value)
+        )
+
+        // assert
+        assertEquals(
+            expected,
+            actual
+        )
+    }
+
+    @Test
+    fun prefix_plus() {
+        // arrange
+        val value = 10000.0
+        val expected = "+10,000円"
+
+        // act
+        val actual = ValueConverter().format(
+            formatter = ValueConverter.Formatter(
+                value = value,
+                valueFormat = ValueFormat(
+                    prefix = ValueFormat.Prefix.Plus,
+                    suffix = ValueFormat.Suffix.Yen
+                )
+            )
         )
 
         // assert
