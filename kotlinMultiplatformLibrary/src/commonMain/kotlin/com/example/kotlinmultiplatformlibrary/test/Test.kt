8a6f402ca47efdf3442de22f43df3383e1faff48
yossibank <yossibank@yahoo.co.jp>
2025-12-02 21:50:18 +0900

    Fix KotlinMultiplatform Library

--- kotlinMultiplatformLibrary/src/commonMain/kotlin/com/example/kotlinmultiplatformlibrary/test/Test.kt
+++ kotlinMultiplatformLibrary/src/commonMain/kotlin/com/example/kotlinmultiplatformlibrary/test/Test.kt
@@ -4,44 +4,4 @@ class Test {
     fun print(): String {
         return "Test"
     }
-
-    fun test1(): String {
-        return "Test1"
-    }
-
-    fun test2(): String {
-        return "Test2"
-    }
-
-    fun test3(): String {
-        return "Test3"
-    }
-
-    fun test4(): String {
-        return "Test4"
-    }
-
-    fun test5(): String {
-        return "Test5"
-    }
-
-    fun test6(): String {
-        return "Test6"
-    }
-
-    fun test7(): String {
-        return "Test7"
-    }
-
-    fun test8(): String {
-        return "Test8"
-    }
-
-    fun test9(): String {
-        return "Test9"
-    }
-
-    fun test10(): String {
-        return "Test10"
-    }
 }
